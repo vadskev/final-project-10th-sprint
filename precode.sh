503016cc100396d6aecb44741703b08934c36a08
Vadim Vaskevich <vadskev@ya.ru>
2024-05-22 11:23:04 +0300

    feat: add code

--- precode.sh
+++ precode.sh
@@ -6,20 +6,27 @@
 #   dir2
 #   dir3
 #       dir4
+mkdir -p task/dir1 task/dir2 task/dir3 task/dir3/dir4
 
 # изменяем текущую директорию на task
+cd task
 
 # создаём пустой файл task/dir2/empty
+touch dir2/empty
 
 # создаём файл task/dir2/hello.sh с таким содержанием:
 # #!/bin/bash
 # echo "$1, привет!"
+printf '#!/bin/bash\necho "$1, привет!"' > dir2/hello.sh
 
 # устанавливаем для task/dir2/hello.sh права rwxrw-r--
+chmod 764 dir2/hello.sh
 
 # сохраняем список файлов task/dir2 в task/dir2/list.txt
+ls dir2 > dir2/list.txt
 
 # копируем содержимое каталога task/dir2 в каталог task/dir3/dir4
+cp -R dir2/* dir3/dir4
 
 # записываем в task/dir1/summary.txt список файлов с расширением *.txt
 # находящихся в task, включая поддиректории
