a4d25a534fdaf81073335b04162c9b0b9eb67471
Vadim Vaskevich <vadskev@ya.ru>
2024-05-22 13:39:37 +0300

    feat: add code

--- precode.sh
+++ precode.sh
@@ -11,6 +11,7 @@ mkdir -p task/dir1 task/dir2 task/dir3 task/dir3/dir4
 # изменяем текущую директорию на task
 cd task
 
+## текущая папка task
 # создаём пустой файл task/dir2/empty
 touch dir2/empty
 
@@ -30,21 +31,32 @@ cp -R dir2/* dir3/dir4
 
 # записываем в task/dir1/summary.txt список файлов с расширением *.txt
 # находящихся в task, включая поддиректории
+##find . "*.txt" > dir1/summary.txt
+find . -name "*.txt" > dir1/summary.txt
 
 # дописываем в task/dir1/summary.txt содержимое task/dir2/list.txt
+cat dir2/list.txt >> dir1/summary.txt
 
 # определяем переменную окружения NAME со значением "Всем студентам"
+export NAME='Всем студентам'
 
 # запускаем task/dir2/hello.sh с переменной окружения NAME в качестве аргумента
 # вывод скрипта должен дописаться в файл task/dir1/summary.txt
+##../dir2/hello.sh "$NAME" >> dir1/summary.txt
+./dir2/hello.sh "$NAME" >> dir1/summary.txt
 
 # перемещаем с переименованием task/dir1/summary.txt в task/Практическое задание
+mv dir1/summary.txt "Практическое задание"
 
 # выводим на консоль содержимое файла task/Практическое задание
+cat "Практическое задание"
 
 # ищем в файле "Практическое задание" строки, которые содержат слово "dir"
 # и затем отсортировываем их
+grep 'dir' "Практическое задание" | sort
 
 # меняем текущую директорию на родительскую для task
+cd ..
 
 # удаляем директорию task со всем содержимым
+rm -r task
